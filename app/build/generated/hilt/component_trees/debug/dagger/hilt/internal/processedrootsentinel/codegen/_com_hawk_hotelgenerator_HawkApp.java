0227fc640d839ee36477a0cc0a493ce37bd0c1f2
h382110229 <382110229@qq.com>
2026-05-05 00:30:43 +0800

    chore: Final cleanup of repository build artifacts and rebranding update

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_hawk_hotelgenerator_HawkApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.hawk.hotelgenerator.HawkApp"
-)
-public final class _com_hawk_hotelgenerator_HawkApp {
-}
